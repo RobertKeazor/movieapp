b54e4095eef60ae97953386dbfb2dafa0f9117de
robertkeazor <robertskeazor@weddingwire.com>
2017-05-29 12:16:24 -0400

    Upgrading the mockito library to Mockito 2, which allows us to Mock the Unmockable ().This is important so we don't have to declare our viewModels as open , just so we can stub it out

--- app/src/test/java/com/example/kotlin/movieapp/ui/main/MainViewModelSpek.kt
+++ app/src/test/java/com/example/kotlin/movieapp/ui/main/MainViewModelSpek.kt
@@ -1,20 +1,32 @@
 package com.example.kotlin.movieapp.ui.main
 
+import com.winterbe.expekt.should
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
-import kotlin.test.assertEquals
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 @RunWith(JUnitPlatform::class)
 class MainViewModelSpek : Spek ({
-    val viewModel = MainViewModel()
+    //TODO This is not a actual ViewModelSpek. Replace with actual Spek, when real app code is introduced
+    describe("mocking with mockito in spek") {
 
-    context("Testing our hello world") {
+        given("mocking Iterable<T> and Iterator<T>") {
+            val mockedIterable = Mockito.mock(Iterable::class.java)
+            val mockedIterator = Mockito.mock(Iterator::class.java)
+            `when`(mockedIterator.hasNext()).thenReturn(true, true, true, true, false)
+            `when`(mockedIterator.next()).thenReturn(0,1,3,4)
+            `when`(mockedIterable.iterator()).thenReturn(mockedIterator)
+            val actual   = mockedIterable.joinToString(",")
+            val expected = "0,1,3,4"
 
-        it("should have our test strings") {
-            assertEquals(viewModel.firstName, "Kotlin 100")
+            it("should iterate mocked values") {
+                actual.should.be.equal(expected)
+            }
         }
     }
 })
