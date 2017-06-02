e015ecbbfb163ab5605b1f572c858d3b2b5b55b3
Umair Javid <ujavid@gmail.com>
2017-06-02 15:14:11 -0400

    Made MainComponent a top level component.

--- app/src/main/java/com/example/kotlin/movieapp/ui/main/MainComponent.kt
+++ app/src/main/java/com/example/kotlin/movieapp/ui/main/MainComponent.kt
@@ -1,9 +1,13 @@
 package com.example.kotlin.movieapp.ui.main
 
+import com.example.kotlin.movieapp.app.AppComponent
 import com.example.kotlin.movieapp.ui.base.BaseComponent
 import com.example.kotlin.movieapp.ui.base.ViewScope
-import dagger.Subcomponent
+import dagger.Component
 
 @ViewScope
-@Subcomponent(modules = arrayOf(MainModule::class))
+@Component(
+        dependencies = arrayOf(AppComponent::class),
+        modules = arrayOf(MainModule::class)
+)
 interface MainComponent : BaseComponent<MainActivity>
