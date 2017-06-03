a1a1006efdc8e57ffb97c8f0dab8dbc1bd3d6351
robertkeazor <robertskeazor@weddingwire.com>
2017-06-03 15:39:21 -0400

    Add Fakers for test

---
+++ app/src/test/java/com/example/kotlin/movieapp/Fakers.kt
@@ -0,0 +1,46 @@
+package com.example.kotlin.movieapp
+
+import com.example.kotlin.movieapp.net.data.MovieData
+import com.example.kotlin.movieapp.net.data.MovieResultData
+
+object Fakers {
+    fun genarateMovieResultData(pageFake: Int = Math.random().toInt(),
+                                resultsFake: List<MovieData> = listOf(generateMovieData()),
+                                totalResultsFake: Int = Math.random().toInt(),
+                                totalPagesFake: Int = Math.random().toInt()) =
+            MovieResultData(
+                    page = pageFake,
+                    results = resultsFake,
+                    totalPages = totalPagesFake,
+                    totalResults = totalResultsFake)
+
+    fun generateMovieData(posterPathFake: String? = "Hello Test",
+                          adultFake: Boolean = false,
+                          overviewFake: String = "Hello Test",
+                          releaseDataFake: String = "Hello Test",
+                          generIdsFake: List<Int> = listOf(5),
+                          idFaker: Int = Math.random().toInt(),
+                          originalTitleFake: String = "Hello Test",
+                          originalLanguageFake: String = "Hello Test",
+                          titleFake: String = "Hello Test",
+                          backdropPathFake: String? = "Hello Test",
+                          popularityFake: Int = Math.random().toInt(),
+                          voteCountFake: Int = Math.random().toInt(),
+                          videoFake: Boolean = false,
+                          voteAverageFake: Number = Math.random()) =
+            MovieData(
+                    posterPath = posterPathFake,
+                    adult = adultFake,
+                    overview = overviewFake,
+                    releaseDate = releaseDataFake,
+                    genreIds = generIdsFake,
+                    id = idFaker,
+                    originalTitle = originalTitleFake,
+                    originalLanguage = originalLanguageFake,
+                    title = titleFake,
+                    backdropPath = backdropPathFake,
+                    popularity = popularityFake,
+                    voteCount = voteCountFake,
+                    video = videoFake,
+                    voteAverage = voteAverageFake)
+}
