5696e42b28d751224998240642d36cdc1218d663
Umair Javid <ujavid@gmail.com>
2017-06-09 16:50:18 -0400

    Updated topRated database call to subscribe on io thread.

--- app/src/main/java/com/example/kotlin/movieapp/repo/MovieDatabaseRepository.kt
+++ app/src/main/java/com/example/kotlin/movieapp/repo/MovieDatabaseRepository.kt
@@ -3,9 +3,11 @@ package com.example.kotlin.movieapp.repo
 import com.example.kotlin.movieapp.db.dao.MovieDao
 import com.example.kotlin.movieapp.db.mapper.MovieEntityMapper
 import com.example.kotlin.movieapp.model.Movie
+import io.reactivex.schedulers.Schedulers
 
 class MovieDatabaseRepository(val movieDao: MovieDao, val movieEntityMapper: MovieEntityMapper) {
     fun topRated() = movieDao.topRated()
+            .subscribeOn(Schedulers.io())
             .map { movieEntityMapper.transform(it) }
 
     fun topRatedAsList() = movieDao.topRatedAsList()
