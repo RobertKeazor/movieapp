291f98c3c5d546a872a416222a71e85a6c2098b2
robertkeazor <robertskeazor@weddingwire.com>
2017-06-13 23:36:30 -0400

    Minor code cleanup and revert id back to Int

--- app/src/main/java/com/example/kotlin/movieapp/adapter/MovieList.kt
+++ app/src/main/java/com/example/kotlin/movieapp/adapter/MovieList.kt
@@ -4,7 +4,6 @@ import android.databinding.ObservableList
 import com.example.kotlin.movieapp.R
 import com.example.kotlin.movieapp.model.Movie
 
-
 class MovieList(items: ObservableList<Movie>): BaseAdapter<Movie>(items) {
     override fun getLayout() = R.layout.item_movie
 
